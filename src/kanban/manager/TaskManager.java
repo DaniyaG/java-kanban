724617baa70ed6be434a44549b37563452ed9761
Daniya Gileva <galieva.danya@yandex.ru>
2025-10-20 17:25:45 +0500

    check style

--- src/kanban/manager/TaskManager.java
+++ src/kanban/manager/TaskManager.java
@@ -9,25 +9,41 @@ import java.util.List;
 public interface TaskManager {
 
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTaskById(int id);
+
     Task createTask(Task newTask);
+
     void updateTask(Task task);
+
     void deleteTaskById(int id);
 
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpicById(int id);
+
     Epic createEpic(Epic newEpic);
+
     void updateEpic(Epic epic);
+
     void deleteEpicById(int id);
 
     List<Subtask> getAllSubtasks();
+
     List<Subtask> getSubtasksByEpicId(int epicId);
+
     void deleteAllSubtasks();
+
     Subtask getSubtaskById(int id);
+
     Subtask createSubtask(Subtask newSubtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtaskById(int id);
 
     List<Task> getHistory();
